8b5214e87a3cd21baf486605702ba04b489c6efa
iAlexeyProkhorov <i.alexey.prokhorov@outlook.com>
2022-07-09 16:50:55 +0300

    Update qBoSlider for nopCommerce 4.30 to version 1.4.1

---
+++ Factories/Admin/SearchModelFactory.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Nop.Plugin.Widgets.qBoSlider.Models.Admin;
+using Nop.Plugin.Widgets.qBoSlider.Service;
+using Nop.Services;
+using Nop.Services.Localization;
+using Nop.Web.Framework.Models;
+using System.Linq;
+
+namespace Nop.Plugin.Widgets.qBoSlider.Factories.Admin
+{
+    /// <summary>
+    /// Represents search model factory interface.
+    /// Stores methods which prepares common search box properties
+    /// </summary>
+    public class SearchModelFactory : ISearchModelFactory
+    {
+        #region Fields
+
+        private readonly ILocalizationService _localizationService;
+        private readonly IWidgetZoneService _widgetZoneService;
+
+        #endregion
+
+        #region Constructor
+
+        public SearchModelFactory(ILocalizationService localizationService,
+            IWidgetZoneService widgetZoneService)
+        {
+            _localizationService = localizationService;
+            _widgetZoneService = widgetZoneService;
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Prepares slide search model
+        /// </summary>
+        /// <param name="model">Slide search model</param>
+        /// <returns>Slide search model</returns>
+        public virtual void PrepareSlideSearchModel<TModel>(TModel model) where TModel : BaseSearchModel, ISlideSearchModel
+        {
+            model.AvailableWidgetZones = _widgetZoneService.GetWidgetZones().Select(x =>
+            {
+                return new SelectListItem()
+                {
+                    Value = x.Id.ToString(),
+                    Text = $"{x.Name} ({x.SystemName})"
+                };
+            }).ToList();
+            model.AvailablePublicationStates = PublicationState.All.ToSelectList(useLocalization: true).ToList();
+            model.SetGridPageSize();
+            model.AvailableWidgetZones.Insert(0,
+                new SelectListItem()
+                {
+                    Value = "0",
+                    Text = _localizationService.GetResource("admin.common.all")
+                });
+        }
+
+        #endregion
+    }
+}
